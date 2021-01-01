4e80b4ac9e949f7cf7fde56b3de50a47d98a3eb7
jeffry zaman <jeffry.zaman@gmail.com>
2020-12-31 21:50:27 -0500

    added user, group, change pw , admin policy

--- customize_resources/iam_user_group.py
+++ customize_resources/iam_user_group.py
@@ -25,13 +25,23 @@ class IAMUserAndGroup(core.Stack):
                                            )
 
         # create a manage policy to attach to the group
-        aws_managed_policy = _iam.ManagedPolicy.from_aws_managed_policy_name(managed_policy_name="AdministratorAccess")
+        _administrative_access = _iam.ManagedPolicy.from_aws_managed_policy_name(
+            managed_policy_name="AdministratorAccess")
+
+        allow_change_pw_value = self.node.try_get_context("change_password_arn")
+        print(f"get allow change pw arn:{allow_change_pw_value}")
+
+        _allow_change_password = _iam.ManagedPolicy.from_managed_policy_arn(
+            self,
+            id="allowChangePasswordID",
+            managed_policy_arn=allow_change_pw_value
+        )
 
         # create a group with the managed policy
         cdk_group = _iam.Group(self,
                                id="cdkGroupID",
                                group_name="cdk_group",
-                               managed_policies=[aws_managed_policy])
+                               managed_policies=[_administrative_access, _allow_change_password])
 
         # create user and add user to the group created above
         user_1 = _iam.User(self,
@@ -39,11 +49,12 @@ class IAMUserAndGroup(core.Stack):
                            groups=[cdk_group],
                            password=user1_secret_password.secret_value,
                            password_reset_required=True,
-                           user_name="cdk_user01")
+                           user_name="cdk_user01",
+                           )
 
         print(f"https://{core.Aws.ACCOUNT_ID}.signin.aws.amazon.com/console")
         user_1_cfn_output = core.CfnOutput(self,
                                            "user1CfnOutout",
                                            description="user01 login url",
-                                           value = f"https://{core.Aws.ACCOUNT_ID}.signin.aws.amazon.com/console")
-                                          #value=f"https://{core.Aws.ACCOUNT_ID}.signin.aws.com/console")
+                                           value=f"https://{core.Aws.ACCOUNT_ID}.signin.aws.amazon.com/console")
+        # value=f"https://{core.Aws.ACCOUNT_ID}.signin.aws.com/console")
