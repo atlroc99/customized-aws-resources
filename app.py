abd486761fa45bc74a2d4387801ff6f4d76e0c8e
jeffry zaman <jeffry.zaman@gmail.com>
2021-01-03 11:53:16 -0500

    updated stack name to match branch name

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ from aws_cdk import core
 from advanced_used_cases.containerized_ms_architecture import MicroserviceWithECSServices
 
 app = core.App()
-MicroserviceWithECSServices(app, "containerized-ms-with-ecs")
+MicroserviceWithECSServices(app, "containerized-micro-services-ecs")
 app.synth()
